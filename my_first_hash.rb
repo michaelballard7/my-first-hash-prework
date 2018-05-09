0eaf1baf121d01a702e57e5edc3e250139361ec2
Michael Ballard <ballard.michael9@gmail.com>
2018-05-09 10:23:47 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -12,9 +12,6 @@ def shipping_manifest
     "porcelain vases" => 2,
     "oil paintings" => 3
   }
-  # fill that hash with key/value pairs that describe the following information: 
-  # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
-
 
 end
 
